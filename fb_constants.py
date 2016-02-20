6b25aa1804a18bbe4ba0817fcb61fd57e836f156
sid802 <sid802@users.noreply.github.com>
2016-02-20 15:40:01 +0200

    Added taggee xpath

--- fb_constants.py
+++ fb_constants.py
@@ -4,13 +4,19 @@ import re
 # Regexes
 class FBRegexes(object):
     my_fid = re.compile(r'"USER_ID":"(?P<result>\d+)')
-    json_from_html = re.compile(r'\{.*\}', re.MULTILINE)
+    json_from_html = re.compile(r'(?P<json>\{.*\})', re.MULTILINE)
+
+    # Likers
     liker_fid_from_url = re.compile(r'php\?id=(?P<result>\d+)')  # url comes from user_fid_url xpath
     liker_username_from_url = re.compile(r'facebook\.com\/(?P<result>[\w.]+)')  # url comes from user_username_url
 
 # Xpaths
 class FBXpaths(object):
-    likers = '//li[@class="fbProfileBrowserListItem"]//a[@data-gt]'
+    # User links
+    user_liker_links = '//li[@class="fbProfileBrowserListItem"]//a[@data-gt]'
+    user_taggee_links = '//a[@class="taggee"]'
+
+    # User link parts
     user_full_name = './text()'  # Relative to liker_info_element
     user_fid_url = './@data-hovercard'  # Relative to liker_info_element
     user_username_url = './@href'
