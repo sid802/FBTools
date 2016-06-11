6cdb78ef91f12e388b804fb3b9339773549d3513
sid802 <sid802@users.noreply.github.com>
2016-06-11 22:28:40 +0300

    Added Group Parser Xpaths

--- parsers/fb_constants.py
+++ parsers/fb_constants.py
@@ -9,6 +9,7 @@ class FBRegexes(object):
     picture_timestamp = re.compile(r'(?P<result>\d+),"text')
 
     url_user = re.compile(r'\.com/(?P<result>[\w.-]+)')
+    url_group_username = re.compile(r'\.com/groups/(?P<result>[\w.-]+)')
 
     # Likers
     liker_fid_from_url = re.compile(r'php\?id=(?P<result>\d+)')  # url comes from user_fid_url xpath
@@ -47,3 +48,10 @@ class FBXpaths(object):
     page_desc_is_split = '//span[@class="text_exposed_show"]'
     page_long_desc_split = '(//div[contains(text(), "Long Desc")]/../../div[2]//*[not(contains(@class,"hide"))]/text())[position() <=2]'
     page_long_desc_unified = '//div[contains(text(), "Long Desc")]/../../div[2]//text()'
+
+    group_title = '//div[@id="fbProfileCover"]//h1/a/text()'
+    group_members_amount = '//span[@id="count_text"]/text()'
+    group_description = '//div[@id="groupsDescriptionBox"]//div[contains(@class,"text_exposed_root")]'
+    group_category = '//div[@id="groupsDescriptionBox"]//div[@class="groupsEditDescriptionArea"]//div[3]/span/text()'
+    group_privacy = '//div[@id="fbProfileCover"]//a[@data-hover]/span/text()'
+    group_member_urls = '//a[@data-hovercard and not(@class)]'
